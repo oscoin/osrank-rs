d32fef8db0140a1bad9e53e1c5c9846abb3d67df
Alfredo Di Napoli <alfredo@monadic.xyz>
2019-07-03 12:24:15 +0200

    Finalise dependency extraction

--- bin/source_dependencies.rs
+++ bin/source_dependencies.rs
@@ -4,9 +4,12 @@ extern crate serde;
 use csv::StringRecord;
 use serde::Deserialize;
 
+use std::collections::HashSet;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::Write;
+
+type ProjectId = u32;
 
 // The order of the fields must be the same of the input file.
 #[derive(Debug, Deserialize)]
@@ -14,31 +17,89 @@ struct Dependency {
     id: u32,
     platform: String,
     project_name: String,
-    project_id: u32,
+    project_id: ProjectId,
     version_number: String,
     version_id: u32,
     dependency_name: String,
     dependency_platform: String,
     dependency_kind: String,
     optional_dependency: bool,
-    dependency_requirements: String,
-    dependency_project_id: String,
+    dependency_requirements: Option<String>,
+    dependency_project_id: Option<ProjectId>,
 }
+
+type UniqueProjects = HashSet<ProjectId>;
 
 fn source_dependencies(path: &str, platform: &str) -> Result<(), Box<dyn Error>> {
     let dependencies_file = File::open(path)?;
 
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::Reader::from_reader(dependencies_file);
+    let mut dependencies = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(format!("data/{}_dependencies.csv", platform.to_lowercase()).as_str())?;
+
+    let mut dependencies_meta = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(format!("data/{}_dependencies_meta.csv", platform.to_lowercase()).as_str())?;
+
+    let mut unique_projects: UniqueProjects = HashSet::new();
+
+    //Write the header(s)
+    dependencies.write(b"FROM_ID,TO_ID\n")?;
+    dependencies_meta.write(b"ID,NAME,PLATFORM\n")?;
 
     for result in rdr
         .records()
         .filter_map(|e| e.ok())
         .filter(by_platform(platform))
-        .take(10)
     {
-        let record: Dependency = result.deserialize(None)?;
-        println!("{:?}", record);
+        let dependency: Dependency = result.deserialize(None)?;
+        extract_dependency(&mut dependencies, &dependency)?;
+        extract_metadata(
+            &mut unique_projects,
+            platform,
+            &mut dependencies_meta,
+            &dependency,
+        )?;
+    }
+
+    Ok(())
+}
+
+fn extract_dependency(
+    dependencies: &mut File,
+    dependency: &Dependency,
+) -> Result<(), Box<dyn Error>> {
+    match dependency.dependency_project_id {
+        None => (),
+        Some(pid) => {
+            dependencies.write(format!("{},{}\n", dependency.project_id, pid).as_bytes())?;
+        }
+    }
+
+    Ok(())
+}
+
+fn extract_metadata(
+    unique_projects: &mut UniqueProjects,
+    platform: &str,
+    dependencies_meta: &mut File,
+    dependency: &Dependency,
+) -> Result<(), Box<dyn Error>> {
+    // Remember if we visited this project before, so that we write only
+    // unique meta information to the file.
+    if let None = unique_projects.get(&dependency.project_id) {
+        unique_projects.insert(dependency.project_id);
+        dependencies_meta.write(
+            format!(
+                "{},{},{}\n",
+                dependency.project_id, dependency.project_name, platform
+            )
+            .as_bytes(),
+        )?;
     }
 
     Ok(())
