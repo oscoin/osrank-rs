349934e62b4180b35b4eb27dfeccdb8f687be3ca
Alfredo Di Napoli <alfredo@monadic.xyz>
2019-08-21 14:48:45 +0200

    Add some tests

--- src/types/walk.rs
+++ src/types/walk.rs
@@ -13,7 +13,7 @@ pub struct RandomWalks<Id>
 where
     Id: Hash + Eq,
 {
-    /// A HashMap between the source of the walk and the walk itself.
+    /// A `HashMap` between the source of the walk and the walk itself.
     random_walks: HashMap<Id, RandomWalk<Id>>,
 }
 
@@ -34,6 +34,10 @@ where
 
     pub fn len(&self) -> usize {
         self.random_walks.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.random_walks.len() == 0
     }
 
     pub fn count_visits(&self, idx: Id) -> usize {
@@ -59,6 +63,9 @@ impl<Id> RandomWalk<Id>
 where
     Id: Clone + Eq + Hash,
 {
+    /// Creates a new `RandomWalk` by passing the source (i.e. beginning)
+    /// of the walk. Note that this also counts as a visit, i.e. it's not
+    /// necessary to call `add_next` after calling `new`.
     pub fn new(source: Id) -> Self {
         let mut m = HashMap::new();
         m.insert(source.clone(), 1);
@@ -68,6 +75,7 @@ where
         }
     }
 
+    /// Adds a segment (typically a graph's node) to the walk.
     pub fn add_next(&mut self, idx: Id) {
         if let Some(visits) = self.random_walk_visits.get_mut(&idx) {
             *visits += 1;
@@ -76,10 +84,70 @@ where
         }
     }
 
+    /// Returns the number of visits of the given segment in the walk.
     pub fn count_visits(&self, idx: &Id) -> NumVisits {
         *self.random_walk_visits.get(idx).unwrap_or(&0)
+    }
+
+    /// Given the `Id` of a segment within the walk, returns the `Id` of the
+    /// source of the walk, if the input `Id` belongs to the walk.
+    pub fn source_from(&self, segment_id: &Id) -> Option<&Id> {
+        if self.random_walk_visits.contains_key(segment_id) {
+            Some(&self.random_walk_source)
+        } else {
+            None
+        }
     }
 }
 
 // Just an alias for now.
 pub type SeedSet = ();
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn random_walk_count_visits_non_existent() {
+        let source = "foo";
+        let w = RandomWalk::new(source);
+        assert_eq!(w.count_visits(&"bar"), 0);
+    }
+
+    #[test]
+    fn random_walk_count_visits_existent() {
+        let source = "foo";
+        let w = RandomWalk::new(source);
+        assert_eq!(w.count_visits(&"foo"), 1);
+    }
+
+    #[test]
+    fn random_walk_add_next() {
+        let source = "foo";
+        let n1 = "bar";
+        let mut w = RandomWalk::new(source);
+        w.add_next(n1);
+        assert_eq!(w.count_visits(&"bar"), 1);
+    }
+
+    #[test]
+    fn random_walk_add_next_twice() {
+        let source = "foo";
+        let n1 = "foo";
+        let mut w = RandomWalk::new(source);
+        w.add_next(n1);
+        assert_eq!(w.count_visits(&"foo"), 2);
+    }
+
+    #[test]
+    fn random_walk_source_from() {
+        let source = "foo";
+        let n1 = "bar";
+        let mut w = RandomWalk::new(source);
+        w.add_next(n1);
+        assert_eq!(w.source_from(&"bar"), Some(&"foo"));
+        assert_eq!(w.source_from(&"baz"), None);
+    }
+
+}
